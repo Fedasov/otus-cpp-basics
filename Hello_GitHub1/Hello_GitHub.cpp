153538a7042f76ced4d0bbacfce56de23fee61e0
Федасов Сергей <fedasov03@inbox.ru>
2023-06-05 12:59:00 +0300

    Добавили файл приветствия Hello_GitHub1 написанный на С++

---
+++ Hello_GitHub1/Hello_GitHub.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+	std:: cout << "Hello Git Hub" << std::endl;
+	return 0;
+    
+}
+
+
+
+// // Definition - определение
+// void todo_move_me_out() {
+// 	std::cout << "This function should be located in the separated source file" << std::endl;
+// }
+
+
+// int main() {
+// 	std::cout << "Hello, World!" << std::endl;
+
+// 	todo_move_me_out();
+	
+// 	return 0;
+// }

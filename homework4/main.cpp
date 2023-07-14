28f4f566599164729174dc7813cc3176f287b585
Fedasov <128252719+Fedasov@users.noreply.github.com>
2023-07-14 23:01:29 +0300

    Add files via upload

---
+++ homework4/main.cpp
@@ -0,0 +1,51 @@
+#pragma once
+#include <iostream>
+#include <limits>
+
+#include "min.cpp"
+#include "stat.cpp"
+#include "max.cpp"
+#include "mean.cpp"
+#include "std.cpp"
+#include "pct90.cpp"
+#include "pct95.cpp"
+
+using namespace std;
+
+int main() {
+
+	const size_t statistics_count = 6;
+	IStatistics *statistics[statistics_count];
+
+	statistics[0] = new Min{};
+	statistics[1] = new Max{};
+	statistics[2] = new Mean{};
+	statistics[3] = new Std{};
+	statistics[4] = new Pct90{};
+	statistics[5] = new Pct95{};
+
+	double val = 0;
+	while (std::cin >> val) {
+		for (size_t i = 0; i < statistics_count; ++i) {
+			statistics[i]->update(val);
+		}
+	}
+
+	// Handle invalid input data
+	if (!std::cin.eof() && !std::cin.good()) {
+		std::cerr << "Invalid input data\n";
+		return 1;
+	}
+
+	// Print results if any
+	for (size_t i = 0; i < statistics_count; ++i) {
+		std::cout << statistics[i]->name() << " = " << statistics[i]->eval() << std::endl;
+	}
+
+	// Clear memory - delete all objects created by new
+	for (size_t i = 0; i < statistics_count; ++i) {
+		delete statistics[i];
+	}
+
+	return 0;
+}
---
+++ homework4/std.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+
+#include "stat.cpp"
+
+using namespace std;
+
+
+
+class Std : public IStatistics {
+public:
+    Std() 
+        : sum{0}
+        , k{0} 
+        , s_std{0}
+        , mean{0} {}
+
+    double std_d(vector<double> arrr, double s, int l) {
+        mean = s / l;
+        for (int i = 0; i < arrr.size(); i++) {
+            s_std += (arrr[i] - mean) * (arrr[i] - mean);
+        }
+        return sqrt(s_std / l);
+    }
+
+	void update(double next) override { 
+        sum += next;
+        k++;
+        arr.push_back(next);
+	}
+
+	double eval() const override {
+        Std s;
+        double c = s.std_d(arr, sum, k);
+		return c;
+	}
+
+	const char * name() const override {
+		return "std";
+	}
+
+private:
+    vector<double> arr;
+    double mean;
+    double s_std;
+    double sum;
+    int k;
+};
